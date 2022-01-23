34b2d2bb13dfde165a7d25ef0b0cfbd97e8b257a
Swaroopaamruthwar <aswaroopa2806@gmail.com>
2022-01-23 10:00:06 -0800

    create_object Assignment

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,12 @@
 function CreateObject(arr) {
     // Write your code here
+    obj={};
+    arr.forEach((element,index,arr)=>{
+        if(index%2===0){
+            obj[element]=arr[index+1];
+        }
+    });
+    return obj;
 }
 
 module.exports = CreateObject;
